5172eb3a20aa86687fa81b54221ba15cf349f3b0
Kirill Nevinskiy <k.nevinskiy@tbank.ru>
2025-02-02 20:28:54 +0500

    refactor: clean up project structure and remove redundant files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Component } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>Star Trek Character Search</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
