195a6a30f53a5aaae02d9004111a8507b6d62141
Kirill Nevinskiy <k.nevinskiy@tbank.ru>
2025-02-23 20:27:24 +0500

    refactor: simplify useLocalStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,25 +1,13 @@
 import { useState } from 'react';
 
-function useLocalStorage<T>(key: string, initialValue: T) {
-  const [storedValue, setStoredValue] = useState<T>(() => {
-    try {
-      const item = localStorage.getItem(key);
-      return item ? (JSON.parse(item) as T) : initialValue;
-    } catch (error) {
-      console.error(error);
-      return initialValue;
-    }
+function useLocalStorage(key: string, initialValue: string) {
+  const [storedValue, setStoredValue] = useState(() => {
+    return localStorage.getItem(key) || initialValue;
   });
 
-  const setValue = (value: T | ((val: T) => T)) => {
-    try {
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
-      setStoredValue(valueToStore);
-      localStorage.setItem(key, JSON.stringify(valueToStore));
-    } catch (error) {
-      console.error(error);
-    }
+  const setValue = (value: string) => {
+    setStoredValue(value);
+    localStorage.setItem(key, value);
   };
 
   return [storedValue, setValue] as const;
